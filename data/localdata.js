d90a47690aacb54a810e7aeb12c3d266cc99af0f
DengXiangshuai <40018621+DengXiangshuai@users.noreply.github.com>
2019-03-23 12:38:13 +0800

    now

---
+++ data/localdata.js
@@ -0,0 +1,13 @@
+var imgUrls = [
+    '/images/iphonex0.jpg',
+    '/images/iphonex1.jpg',
+    '/images/iphonex2.jpg',
+    '/images/iphonex3.jpg'
+  ]
+
+
+
+
+  module.exports = {
+    idexData: imgUrls
+  }
---
+++ pages/index/index.js
@@ -0,0 +1,26 @@
+var dataIndex = require("../../data/localdata.js");
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+   
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      images: dataIndex.idexData
+    })
+  },
+  getCurrent: function (e) {
+   // console.log(e.detail.current);
+    var swiperId = e.detail.current
+  } 
+
+
+})
