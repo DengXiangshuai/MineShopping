7482b14cd974ec0477d0ec7b945802ecb3079248
DengXiangshuai <40018621+DengXiangshuai@users.noreply.github.com>
2019-03-23 15:18:09 +0800

    设置tabbar

--- pages/firstshop/firstshop.js
+++ pages/firstshop/firstshop.js
@@ -1,4 +1,5 @@
 // pages/firstshop/firstshop.js
+var dataIndex = require("../../data/localdata.js");
 Page({
 
   /**
@@ -12,7 +13,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      images: dataIndex.indexData,
+      Index: dataIndex.index
+    })
   },
 
   /**
